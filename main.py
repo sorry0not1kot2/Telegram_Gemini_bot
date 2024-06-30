9404146c56837f181e6f2b4671495c5163768b5b
sorry0not1kot2 <162019473+sorry0not1kot2@users.noreply.github.com>
2024-06-30 21:39:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,10 @@ async def main():
 if __name__ == '__main__':
     try:
         asyncio.run(main())
+    except RuntimeError as e:
+        logger.error(f"Runtime error: {str(e)}")
     finally:
-        loop = asyncio.get_event_loop()
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
         loop.run_until_complete(loop.shutdown_asyncgens())
         loop.close()
