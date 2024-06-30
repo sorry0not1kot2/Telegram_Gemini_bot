d798774a23b123a7e885aa4c96650d9c5f6f329a
sorry0not1kot2 <162019473+sorry0not1kot2@users.noreply.github.com>
2024-06-30 21:35:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -54,4 +54,9 @@ async def main():
     await application.run_polling()
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    finally:
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(loop.shutdown_asyncgens())
+        loop.close()
