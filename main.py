688b360d82293b0159736c58f777528fa81275d5
sorry0not1kot2 <162019473+sorry0not1kot2@users.noreply.github.com>
2024-07-02 01:28:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -26,24 +26,13 @@ async def get_bot_username():
     bot_info = await bot.get_me()
     return bot_info.username
 
-# Функция экранирования специальных символов MarkdownV2
-def escape_markdown_v2(text):
-    # Список специальных символов, которые нужно экранировать в MarkdownV2
-    escape_chars = '_*~`>#+-=|{}.!\\)('
-    # Экранирование символов
-    return ''.join(['\\' + char if char in escape_chars else char for char in text])
-
 async def get_gemini_response(query):
     logger.info(f"Sending query to Gemini: {query}")
     try:
         response = model.generate_content(query)
         response_text = response.candidates[0].content.parts[0].text
-        # Замена трех одинарных кавычек на три обратных апострофа
-        # response_text = response_text.replace("'''", "```")
-        # Экранирование специальных символов для MarkdownV2
-        escaped_response = escape_markdown_v2(response_text)
-        logger.info(f"Received response from Gemini: {escaped_response}")
-        return escaped_response
+        logger.info(f"Received response from Gemini: {response_text}")
+        return response_text
     except Exception as e:
         logger.error(f"Error getting response from Gemini: {str(e)}")
         return f"Произошла ошибка при обращении к Gemini: {str(e)}"
@@ -54,20 +43,16 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     bot_username = await get_bot_username()
 
     if message.reply_to_message and message.reply_to_message.from_user.username == bot_username:
-        # Если сообщение является ответом на сообщение бота
         logger.info(f"Processing reply to bot: {query}")
     elif f"@{bot_username}" in query:
-        # Если сообщение содержит упоминание бота
         logger.info(f"Processing mention of bot: {query}")
         query = query.replace(f"@{bot_username}", "").strip()
     else:
-        # Игнорируем сообщения, не содержащие упоминание бота или не являющиеся ответом на сообщение бота
         return
 
     try:
         response = await get_gemini_response(query)
-        # Отправка сообщения с форматированием MarkdownV2
-        await message.reply_text(response, parse_mode='MarkdownV2')
+        await message.reply_text(response)
     except Exception as e:
         await message.reply_text(f"Произошла ошибка: {str(e)}")
 
