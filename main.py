c1bb968973cc6fe6e771f924746c7cc2262d4659
sorry0not1kot2 <162019473+sorry0not1kot2@users.noreply.github.com>
2024-07-06 02:51:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ generation_config = {
     "temperature": 0.5,
     "top_p": 0.95,
     "top_k": 64,
-    "max_output_tokens": 4096,
+    "max_output_tokens": 8192,
 }
 
 model = genai.GenerativeModel(model_name="gemini-1.5-flash")
