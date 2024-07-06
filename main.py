34b6961f81843dd427ab6a399b8b53d351554950
sorry0not1kot2 <162019473+sorry0not1kot2@users.noreply.github.com>
2024-07-06 15:49:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -158,8 +158,8 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
              "Я -  бот на основе Gemini-flesh.\n\n"
              "Для общения со мной, называйте меня в сообщении \n"
              f"по @{bot_username} или  сделайте ответ (replay) на мои сообщения, чтобы я вам ответил. \n\n"
-             " команда `/start` - запуск бота"
-             " команда `/clear` - удаление истории сообщений"
+             " команда `/start` - запуск бота\n"
+             " команда `/clear` - удаление истории сообщений\n\n"
              "Я общаюсь только в телеграм-группе Беседка...\n\n"
              "© @Don_Dron",
         message_thread_id=update.effective_message.message_thread_id
